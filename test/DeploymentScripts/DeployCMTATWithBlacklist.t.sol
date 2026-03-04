91f9d3a450fbdeda422db8a0a6ff9bd59e258161
Ryan Sauge <71391932+rya-sge@users.noreply.github.com>
2026-03-04 10:23:17 +0100

    Add test with deployment script

---
+++ test/DeploymentScripts/DeployCMTATWithBlacklist.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MPL-2.0
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {IRuleEngine} from "CMTAT/interfaces/engine/IRuleEngine.sol";
+import {CMTATStandalone} from "CMTAT/deployment/CMTATStandalone.sol";
+import {RuleBlacklist} from "src/rules/validation/deployment/RuleBlacklist.sol";
+import {DeployCMTATWithBlacklist} from "script/DeployCMTATWithBlacklist.s.sol";
+
+contract DeployCMTATWithBlacklistTest is Test {
+    function testDeployCMTATWithBlacklist() public {
+        DeployCMTATWithBlacklist script = new DeployCMTATWithBlacklist();
+        (CMTATStandalone token, RuleBlacklist rule) = _deploy(script);
+
+        assertEq(address(token.ruleEngine()), address(rule));
+    }
+
+    function _deploy(DeployCMTATWithBlacklist script)
+        internal
+        returns (CMTATStandalone token, RuleBlacklist rule)
+    {
+        (token, rule) = script.deploy(address(1), address(0));
+    }
+}
